3264c8219247a88bc7fe27e77e7f570f352657e4
hechaohe <550121308@qq.com>
2017-01-19 09:35:50 +0800

    Initial Commit

---
+++ test_summary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test_summary
+//
+//  Created by hc on 2017/1/19.
+//  Copyright © 2017年 hc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test_summary/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test_summary
+//
+//  Created by hc on 2017/1/19.
+//  Copyright © 2017年 hc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
